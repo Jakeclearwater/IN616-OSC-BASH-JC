7e8fd6418d0410b0ff6c5d4ef7fd51832f45bda3
Jake Clearwater <83677506+Jakeclearwater@users.noreply.github.com>
2022-05-03 18:46:30 +1200

    updated file from notepad file

--- readCsv.sh
+++ readCsv.sh
@@ -1,57 +1,54 @@
 #!/bin/bash
-filename=$1
 
+filename=$1
 IFS=";"
 
-sed '1d' $filename | while read Email DoB Groups Sfolder
-do echo "Email" $Email
-	email=$(echo $Email | cut -d '@' -f1)
-	echo $email
-	firstval=$(echo $email | cut -d '.' -f1)
-	firstletter=${firstval:0:1}
-	secondval=$(echo $email | cut -d '.' -f2)
-	username=$(echo "$firstletter""$secondval")
-	echo "Username" $username
-   echo "DoB" $DoB
-   	year=$(echo $DoB | cut -d '/' -f1)
-	month=$(echo $DoB | cut -d '/' -f2)
-	password=$(echo "$month""$year")
-	echo $password
-   echo "Groups" $Groups
-   echo "Sfolder" $Sfolder
+sudo groupadd visitor
+sudo groupadd staff
 
-   if [ $Groups = "staff" ]
+sudo mkdir -p /sharedFolders
+sudo mkdir -p /sharedFolders/staffdata
+sudo mkdir -p /sharedFolders/visitordata
+
+sed 1d $filename | while read email DoB Groups Sfolder
+do
+   echo "======================================"
+   echo "email:" $email
+   #creating username from email
+   email=$(echo $email | cut -d '@' -f1)
+   val=$(echo $email | cut -d '.' -f1)
+   second=$(echo $email | cut -d '.' -f2)
+   firstletter=${email:0:1}
+   username=$(echo "$firstletter""$second")
+   #creating password from Date of Birth
+   passval=$(echo $DoB | cut -d '/' -f2)
+   passval2=$(echo $DoB | cut -d '/' -f1)
+   password=$(echo "$passval""$passval2")
+   #creating users and groups
+   echo "password:" "**"$passval2
+   echo "Username:"  $username
+   echo "DoB:" $DoB
+   echo "Groups:" $Groups
+   echo "Sfolder:" $Sfolder
+   echo "======================================"
+
+   sudo useradd -d /home/${username} -m -s /bin/bash $username
+
+   sudo passwd $username
+
+   echo $username:$password |sudo chpasswd
+
+
+
+   sudo chage --lastday 0 ${username}
+
+   if [ -z "$Groups" ];
    then
-	 sudo useradd -g staff $username
-	 sudo passwd $username
-	 echo $password
-	elif [ $Groups = "sudo" ]
-	then
-		sudo useradd -g sudo $username
-		sudo passwd $username
-		echo $password
-	elif [ $Groups = "visitor" ]
-	then
-		sudo useradd -g visitor $username
-		sudo passwd $username
-		echo $password
-	elif [ $Groups = "sudo, visitor" ]
-	then
-		sudo useradd -G sudo,visitor $username
-		sudo passwd $username
-		echo $password
-	elif [ $Groups = "sudo, staff" ]
-	then
-		sudo useradd -G sudo,staff $username
-		sudo passwd $username
-		echo $password
-	elif [ $Groups =" " ]
-	then
-		sudo useradd $username
-		sudo passwd $username
-		echo $password
+           echo "Empty"
+   else
+           sudo usermod -aG $Groups $username
+   fi
 
 
-fi
- 
+
 done < $filename
