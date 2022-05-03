ee81dadb1a9f514603d71b46f0e40e6ca06ad57f
Jake Clearwater <83677506+Jakeclearwater@users.noreply.github.com>
2022-05-03 20:23:10 +1200

    updated file with functions to create dirs

--- readCsv.sh
+++ readCsv.sh
@@ -1,54 +1,63 @@
 #!/bin/bash
 
 filename=$1
-IFS=";"
 
-sudo groupadd visitor
-sudo groupadd staff
+sharedFolders(){
+	sudo groupadd visitor
+	sudo groupadd staff
 
-sudo mkdir -p /sharedFolders
-sudo mkdir -p /sharedFolders/staffdata
-sudo mkdir -p /sharedFolders/visitordata
+	sudo mkdir /sharedFolders
+	sudo mkdir /sharedFolders/staffData
+	sudo mkdir /sharedFolders/visitorData
 
-sed 1d $filename | while read email DoB Groups Sfolder
-do
-   echo "======================================"
-   echo "email:" $email
-   #creating username from email
-   email=$(echo $email | cut -d '@' -f1)
-   val=$(echo $email | cut -d '.' -f1)
-   second=$(echo $email | cut -d '.' -f2)
-   firstletter=${email:0:1}
-   username=$(echo "$firstletter""$second")
-   #creating password from Date of Birth
-   passval=$(echo $DoB | cut -d '/' -f2)
-   passval2=$(echo $DoB | cut -d '/' -f1)
-   password=$(echo "$passval""$passval2")
-   #creating users and groups
-   echo "password:" "**"$passval2
-   echo "Username:"  $username
-   echo "DoB:" $DoB
-   echo "Groups:" $Groups
-   echo "Sfolder:" $Sfolder
-   echo "======================================"
+	sudo chgrp staff /sharedFolders/staffData
+	sudo chgrp visitor /sharedFolders/visitorData
+	sudo chmod g-rwx /sharedFolders/staffData
+	sudo chmod g-rwx /sharedFolders/visitorData
+}
 
-   sudo useradd -d /home/${username} -m -s /bin/bash $username
+createUsers() {
 
-   sudo passwd $username
+	IFS=";"
+	#removes headers before reading
+	sed 1d $filename | while read col1 col2 col3 col4
+	do
+	   Email=$($col1)
+	   DoB=$($col2)
+	   Groups=$($col3)
+	   Sfolder=$($col4)
+	   echo "email:" $email
+	   #create username from email
+	   email=$(echo $Email | cut -d '@' -f1)
+	   val=$(echo $email | cut -d '.' -f1)
+	   second=$(echo $email | cut -d '.' -f2)
+	   firstVal=${email:0:1}
+	   username=$(echo "$firstVal""$second")
+	   #create password from DoB
+	   month=$(echo $DoB | cut -d '/' -f2)
+	   year=$(echo $DoB | cut -d '/' -f1)
+	   password=$(echo "$month""$year")
+	   #create users and groups
+	   echo "password:" $password
+	   echo "Username:"  $username
+	   echo "DoB:" $DoB
+	   echo "Groups:" $Groups
+	   echo "Sharedfolder:" $Sfolder
+	   echo "----------------------------------------"
 
-   echo $username:$password |sudo chpasswd
+	   sudo useradd -d /home/${username} -m -s /bin/bash $username
 
+	   echo $username:$password |sudo chpasswd
 
+	   sudo chage --lastday 0 ${username}
 
-   sudo chage --lastday 0 ${username}
+	   if [ -z "$Groups" ];
+	   then
+			   echo "Groups is Empty"
+	   else
+			   sudo usermod -aG $Groups $username
+	   fi
 
-   if [ -z "$Groups" ];
-   then
-           echo "Empty"
-   else
-           sudo usermod -aG $Groups $username
-   fi
-
-
+}
 
 done < $filename
