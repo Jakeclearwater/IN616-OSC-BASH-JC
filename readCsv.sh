1eec83c97962a58ec6d4ba783a436e21ec1c01bf
Jake <jakeclearwater7@gmail.com>
2022-05-04 07:49:12 +0000

    finished task 1, with Aliases and links

--- readCsv.sh
+++ readCsv.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 filename=$1
-
+if [[$filename == http* ]];
+then
+	wget -gO users.csv $filename
+fi
+sudo touch logfile.log
+#logging is not quite working properly
 
 sharedFolders(){
 	sudo groupadd visitor
 	sudo groupadd staff
 
-	sudo mkdir /sharedFolders
-	sudo mkdir /sharedFolders/staffData
-	sudo mkdir /sharedFolders/visitorData
+	sudo mkdir /home/sharedFolders
+	sudo mkdir /home/sharedFolders/staffData >> logfile.log
+	sudo mkdir /home/sharedFolders/visitorData >> logfile.log
 
-	sudo chgrp staff /sharedFolders/staffData
-	sudo chgrp visitor /sharedFolders/visitorData
-	sudo chmod g-rwx /sharedFolders/staffData
-	sudo chmod g-rwx /sharedFolders/visitorData
+	sudo chgrp staff /home/sharedFolders/staffData
+	sudo chgrp visitor /home/sharedFolders/visitorData
+	sudo chmod g-rwx /home/sharedFolders/staffData
+	sudo chmod g-rwx /home/sharedFolders/visitorData
 }
 
 createUsers() {
@@ -26,7 +31,8 @@ createUsers() {
 	do
 	if [ $index -gt  0 ] 
 	then
-	   echo "email:" $Email
+	echo "User successfully created" >> logfile.log
+	   echo "email:" $Email  >> logfile.log
 	   #create username from email
 	   email=$(echo $Email | cut -d '@' -f1)
 	   val=$(echo $email | cut -d '.' -f1)
@@ -38,13 +44,13 @@ createUsers() {
 	   year=$(echo $DoB | cut -d '/' -f1)
 	   password=$(echo "$month""$year")
 	   #create users and groups
-	   echo "password:" $password
-	   echo "Username:"  $username
-	   echo "DoB:" $DoB
-	   echo "Groups:" $Groups
-	   echo "Sharedfolder:" $Sfolder
+	   echo "password:" $password  >> logfile.log
+	   echo "Username:"  $username  >> logfile.log
+	   echo "DoB:" $DoB  >> logfile.log
+	   echo "Groups:" $Groups  >> logfile.log
+	   echo "Sharedfolder:" $Sfolder  >> logfile.log
 
-	   sudo useradd -d /home/${username} -m -s /bin/bash $username
+	   sudo useradd -d /home/${username} -m -s /bin/bash $username >> logfile.log
 
 	   echo $username:$password |sudo chpasswd
 
@@ -52,25 +58,40 @@ createUsers() {
 
 	   if [ -z "$Groups" ];
 	   then
-			   echo "User Has no Assigned Group/s"
+			   echo "User Has no Assigned Group/s" >> logfile.log
 	   else
 			   sudo usermod -aG $Groups $username
 	   fi
-	   echo "----------------------------------------"
 	
+	   if grep -q "sudo" <<< "$Groups";
+	   then
+		   alias myls="ls -la"
+		   sudo touch ~/.bash_aliases
+		   echo alias myls="ls -la" >> ~/.bash_aliases
+	   fi
+
+	   if grep -q "staffData" <<< "$Sfolder";
+	   then
+		   sudo ln -s /home/$username/sharedFolders/staffData /home/$username/shared
+	   elif grep -q "visitorData" <<< "$Sfolder";
+	   then
+		   sudo ln -s /home/$username/sharedFolders/visitordata /home/$username/shared
+	   fi
+	   echo "----------------------------------------"
+
 	fi
 	(( index++ ))
 
 done < $filename
-
+echo "Total Users: $index" >> logfile.log
 }
 
 sharedFolders
 createUsers
 
 deleteFolders() {
-	sudo rm -r /sharedFolders/staffData
-	sudo rm -r /sharedFolders/visitorData
+	sudo rm -r /home/sharedFolders/staffData
+	sudo rm -r /home/sharedFolders/visitorData
 }
 
 deleteUsers() {
@@ -90,4 +111,4 @@ deleteAll() {
 	deleteFolders
 	deleteUsers
 }
-deleteAll
+#deleteAll
